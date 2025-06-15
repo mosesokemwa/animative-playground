f989612c348f30d9ee41b37d1d0b4ed5031c8711
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2025-06-15 12:35:54 +0300

    Update version to 0.0.6

--- scripts/check-version.js
+++ scripts/check-version.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 const packageJson = require("../package.json");
+const appJson = require("../app.json");
+
 const appJsonPath = "./app.json";
+const packageJsonPath = "./package.json";
+const packageJsonLockPath = "./package-lock.json";
 
 const currentVersion = packageJson.version;
 let [major, minor, patch] = currentVersion.split(".");
@@ -31,16 +35,26 @@ if (patch === 9) {
 
 if (newVersion !== currentVersion) {
   packageJson.version = newVersion;
-  fs.writeFileSync("./package.json", JSON.stringify(packageJson, null, 2));
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
   console.log(`Version updated to ${newVersion}`);
 } else {
   console.log("No version update required");
 }
 
+// Update app.json expo.version
+const currentAppVersion = appJson.expo.version;
+if (currentAppVersion !== newVersion) {
+  appJson.expo.version = newVersion;
+  fs.writeFileSync(appJsonPath, JSON.stringify(appJson, null, 2));
+  console.log(`app.json version updated to ${newVersion}`);
+} else {
+  console.log("No app.json version update required");
+}
+
 // git commit version update
 const { exec } = require("child_process");
 exec(
-  'git add package.json && git commit -m "Update version to ' +
+  'git add package.json app.json && git commit -m "Update version to ' +
     newVersion +
     '"',
   (error, stdout, stderr) => {
